34dda1e11735ded5bc48891bac358c8c9b9cf159
Zhengan Chen <zhenganc@kth.se>
2026-09-01 20:31:38 +0200

    fix

--- lab1-skel/task2/MainC.java
+++ lab1-skel/task2/MainC.java
@@ -1,22 +1,20 @@
 public class MainC {
 
+    static volatile int sharedInt = 0;
+    static volatile boolean done = false;
 
-		static volatile int sharedInt=0;
-    	static volatile boolean done=false;
+    public static class Incrementer implements Runnable {
 
+        private final Object object;
 
-	public static class Incrementer implements Runnable {
-
-
-		private final Object object;
-        public Incrementer(Object obj){
+        public Incrementer(Object obj) {
             this.object = obj;
 
         }
 
-		public void run() {
+        public void run() {
 
-			synchronized(this.object){
+            synchronized (this.object) {
                 for (int i = 0; i < 1_000_000; i++) {
                     sharedInt++;
                 }
@@ -24,69 +22,61 @@ public class MainC {
                 done = true;
                 object.notify();
 
-            }   
+            }
 
-		}
-	}
+        }
+    }
 
-	public static class Printer implements Runnable {
+    public static class Printer implements Runnable {
 
-		private final Object object;
-        public Printer(Object obj){
+        private final Object object;
+
+        public Printer(Object obj) {
             this.object = obj;
 
         }
 
-		public void run() {
-			synchronized(this.object){
+        public void run() {
+            synchronized (this.object) {
 
-            while(!done){
-                try{
-                    object.wait();
-                }catch(InterruptedException e) {
-                    e.printStackTrace();
+                while (!done) {
+                    try {
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-            System.out.println("sharedInt: " + sharedInt);
+                System.out.println("sharedInt: " + sharedInt);
 
             }
-            
 
-		}
-	}
+        }
+    }
 
-	public static void main(String [] args) {
+    public static void main(String[] args) {
 
+        int totalRuns = 30;
 
+        for (int i = 0; i < totalRuns; i++) {
+            sharedInt = 0;
+            done = false;
 
-		int totalRuns = 30;
-        
+            final Object obj = new Object();
+            Thread incrementingThread = new Thread(new Incrementer(obj));
+            Thread printingThread = new Thread(new Printer(obj));
 
-        for(int i =0;i<totalRuns;i++){
-            sharedInt=0;
-            done=false;
+            incrementingThread.start();
+            printingThread.start();
 
-        final Object obj = new Object();
-        Thread incrementingThread = new Thread(new Incrementer(obj));
-        Thread printingThread = new Thread(new Printer(obj));
+            try {
+                incrementingThread.join();
+                printingThread.join();
 
-        incrementingThread.start();
-        printingThread.start();
-
-        try{
-            incrementingThread.join();
-            printingThread.join();
-
-        }catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-        
-
         }
 
-
-
-
-	}
+    }
 }
