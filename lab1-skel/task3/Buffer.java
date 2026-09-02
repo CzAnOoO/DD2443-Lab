b6105762338d740ec0b9b33a509d4a6c13216ad1
41w1 <111567743+Dawacode@users.noreply.github.com>
2026-09-02 20:18:18 +0200

    task 3 complete
    task 3 is now done

--- lab1-skel/task3/Buffer.java
+++ lab1-skel/task3/Buffer.java
@@ -1,19 +1,113 @@
-public class Buffer {
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+	public class Buffer {
+
+	private final Queue<Integer> queue = new ArrayDeque<>();
+	private final ReentrantLock lock = new ReentrantLock();
+	private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
+
+	private int capacity = 0;
+	private int putIndex = 0;
+    private int takeIndex = 0;
+	private boolean closed;
+	private int items;
 
 	public Buffer(int size) {
 		// TODO
-	}
+			this.capacity = size;	
+		}
 
 	void add(int i) {
 		// TODO
+		lock.lock();
+		try {
+			
+			if (closed) {
+                throw new IllegalStateException("Buffer is closed.");
+            }
+
+			// while the list is full and not closed, wait until not full 
+			while (!closed && queue.size()==capacity){
+				try {
+                    notFull.await();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException("Thread interrupted while waiting to add", e);
+                }
+			}
+
+			// if close and empty throw exceptions 
+			if (closed) {
+                throw new IllegalStateException("Buffer was closed while waiting to add.");
+            }
+
+			// insert 
+			queue.add(i);
+			notEmpty.signal();
+
+		} finally{
+
+			lock.unlock();
+
+		}
+
+
 	}
+
 
 	public int remove() {
 		// TODO
-		return -1;
+
+			lock.lock();
+
+			try {
+				
+				while (queue.isEmpty() && !closed) {
+					try {
+                    	notEmpty.await();
+                	} catch (InterruptedException e) {
+                    	Thread.currentThread().interrupt();
+    					throw new RuntimeException("Thread was interrupted while waiting to remove", e);
+                	}          
+				}	
+
+				if(queue.isEmpty() && closed){
+					throw new IllegalStateException("Buffer was closed while waiting to remove.");
+	
+				}
+
+				int item=queue.remove();
+				notFull.signal();
+				return item;
+
+
+			} finally {
+				lock.unlock();
+			}
 	}
 
 	public void close() {
+				
 		// TODO
+		lock.lock();
+		try {
+			if(closed){
+				throw new IllegalStateException("Buffer is already closed.");
+			}
+			closed=true;
+			notFull.signalAll();
+            notEmpty.signalAll();
+		} finally {
+
+			lock.unlock();
+
+		}
+	
+	
+	
 	}
 }
--- lab1-skel/task3/Main.java
+++ lab1-skel/task3/Main.java
@@ -1,14 +1,56 @@
 public class Main {
 	public static class Producer implements Runnable {
+		
+		private final Buffer buffer;
+		public Producer(Buffer buffer){
+			this.buffer= buffer;
+		}
+		
 		public void run() {
+				for (int i = 0; i < 1_000_000; i++) {
+					buffer.add(i);
+            	}
+				buffer.close();
+			
 		}
 	}
 
 	public static class Consumer implements Runnable {
+		 
+		private final Buffer buffer;
+
+		public Consumer(Buffer buffer){
+				this.buffer = buffer;
+		}
+		
 		public void run() {
+
+			try {
+            while (true) {
+                int item = buffer.remove();
+                System.out.println("This is the removed integer : " + item);
+            }
+        	} catch (IllegalStateException e) {
+            // Buffer closed and completely drained -> exit loop cleanly
+        	}
 		}
 	}
 
 	public static void main(String [] args) {
+
+		Buffer buffer = new Buffer(100);
+		Thread producerThread = new Thread(new Producer(buffer));
+        Thread consumThread = new Thread(new Consumer(buffer));
+
+		consumThread.start();
+		producerThread.start();
+		 try{
+            producerThread.join();
+            consumThread.join();
+
+        }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
 	}
 }
