fb65380e3fcba70901879fb5095ba5bc24c5c45f
Zhengan Chen <zhenganc@kth.se>
2026-09-02 17:38:26 +0200

    lab1 Task4-1

--- lab1-skel/task4/CountingSemaphore.java
+++ lab1-skel/task4/CountingSemaphore.java
@@ -1,14 +1,39 @@
 public class CountingSemaphore {
 
+	volatile int semaphore = 0;
+	public int n;
+
+	public int getSemaphore() {
+		synchronized (this) {
+			return semaphore;
+		}
+	}
+
 	public CountingSemaphore(int n) {
 		// TODO
+		this.n = n;
 	}
 
-	public void signal() {
+	public void signal() throws InterruptedException {
 		// TODO
+		synchronized (this) {
+			semaphore++;
+			if (semaphore > n) {
+				semaphore--;
+				wait();
+			} else if (semaphore - 1 < 0) {
+				notify();
+			}
+		}
 	}
 
-	public void s_wait() {
+	public void s_wait() throws InterruptedException {
 		// TODO
+		synchronized (this) {
+			semaphore--;
+			if (semaphore < 0) {
+				wait();
+			}
+		}
 	}
 }
--- lab1-skel/task4/Main.java
+++ lab1-skel/task4/Main.java
@@ -1,10 +1,48 @@
 public class Main {
 
+	static volatile int semaphore = 0;
+
 	public static class Runner implements Runnable {
+
+		CountingSemaphore semaphore;
+
+		public void setSemaphore(CountingSemaphore s) {
+			this.semaphore = s;
+		}
+
 		public void run() {
+			try {
+				semaphore.signal();
+				System.out.println("Thread start:" + Thread.currentThread().threadId() + "start");
+				Thread.sleep(100);
+				System.out.println("Thread: " + Thread.currentThread().threadId() + "finish");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public static void main(String [] args) {
+	public static void main(String[] args) throws InterruptedException {
+		int n = 3;
+		int x = 5;
+		CountingSemaphore semaphore = new CountingSemaphore(n);
+
+		Runner r = new Runner();
+		r.setSemaphore(semaphore);
+
+		Thread ts[] = new Thread[x];
+
+		for (int i = 0; i < x; i++) {
+			ts[i] = new Thread(r);
+			ts[i].start();
+		}
+		Thread.sleep(50);
+		System.out.println("Current semaphore: " + semaphore.getSemaphore());
+
+		for (int i = 0; i < x; i++) {
+			ts[i].join();
+		}
+
 	}
 }
