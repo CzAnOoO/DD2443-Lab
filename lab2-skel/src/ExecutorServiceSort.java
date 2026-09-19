908433ffd988ef27a48196b0179b5e0a12190833
Zhengan Chen <zhenganc@kth.se>
2026-09-19 12:28:45 +0200

    lab2 Task3

--- lab2-skel/src/ExecutorServiceSort.java
+++ lab2-skel/src/ExecutorServiceSort.java
@@ -1,9 +1,11 @@
+
 /**
  * Sort using Java's ExecutorService.
  */
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ExecutorServiceSort implements Sorter {
         public final int threads;
@@ -13,7 +15,26 @@ public class ExecutorServiceSort implements Sorter {
         }
 
         public void sort(int[] arr) {
-                // TODO: sort arr.
+                // TODO Implement your sorting algorithm.
+                if (arr == null || arr.length <= 1) {
+                        return;
+                }
+                // This will initiate the sort
+                ExecutorService pool = Executors.newFixedThreadPool(threads);
+
+                // https://stackoverflow.com/questions/3305059/how-do-you-calculate-log-base-2-in-java-for-integers
+                int deepT = 31 - Integer.numberOfLeadingZeros(threads);
+
+                Future<?> start = pool.submit(new Worker(arr, 0, arr.length - 1, pool, deepT));
+
+                try {
+                        start.get();
+                } catch (Exception e) {
+                        // TODO: handle exception
+                }
+
+                pool.shutdown();
+
         }
 
         public int getThreads() {
@@ -21,10 +42,85 @@ public class ExecutorServiceSort implements Sorter {
         }
 
         private static class Worker implements Runnable {
-                Worker() {
+                int[] array;
+                int low;
+                int high;
+                ExecutorService pool;
+                int deepT;
+
+                Worker(int[] array, int low, int high, ExecutorService pool, int deepT) {
+                        this.array = array;
+                        this.low = low;
+                        this.high = high;
+                        this.pool = pool;
+                        this.deepT = deepT;
                 }
 
                 public void run() {
+                        quicksort(array, low, high, deepT);
+                }
+
+                // https://www.youtube.com/watch?v=Hoixgm4-P4M
+                private void swap(int[] array, int a, int b) {
+                        int temp = array[a];
+                        array[a] = array[b];
+                        array[b] = temp;
+                }
+
+                public int partition(int[] array, int low, int high) {
+                        // Pick the last element as pivot (or median of three)
+                        // int middle = low + (high - low) / 2;
+                        int pivot = array[high];
+                        // swap(array,middle,high);
+
+                        // Boundary pointer for elements <= pivot
+                        int i = low - 1;
+
+                        // Scan all elements up to high - 1
+                        for (int j = low; j < high; j++) {
+                                if (array[j] <= pivot) {
+                                        i++;
+                                        swap(array, i, j); // Move smaller element to the left region
+                                }
+                        }
+
+                        // Put the pivot in its permanent sorted spot right after the left region
+                        swap(array, i + 1, high);
+
+                        // Return pivot's final index
+                        return i + 1;
+                }
+
+                public void quicksort(int[] array, int low, int high, int deepT) {
+
+                        if (low >= high) {
+                                return;
+                        }
+                        // Create two treads 
+                        // ExecutorService executor = Executors.newFixedThreadPool(2);
+
+                        // Step 1: Pick a pivot and rearrange elements around it in-place
+                        int pivot_index = partition(array, low, high);
+
+                        if (deepT > 0) {
+                                deepT--;
+                                // Step 2: Recurse on the left sub-array (elements before pivot)
+                                Future<?> left = pool.submit(new Worker(array, low, pivot_index - 1, pool, deepT));
+
+                                try {
+                                        left.get();
+                                        // right.get();
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                        // TODO: handle exception
+                                }
+                        } else {
+                                quicksort(array, low, pivot_index - 1, deepT);
+                        }
+
+                        // Step 3: Recurse on the right sub-array (elements after pivot)
+                        // Future<?> right = pool.submit(new Worker(array, pivot_index + 1, high, pool));
+                        quicksort(array, pivot_index + 1, high, deepT);
                 }
         }
 }
